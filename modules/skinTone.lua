6fad9aa7385126973faa5671fbcd921e1515f9bf
Domkaq <164402886+Domkaq@users.noreply.github.com>
2026-03-12 00:38:33 +0100

    Add skin tone settings module

---
+++ modules/skinTone.lua
@@ -0,0 +1,24 @@
+local skinToneSettings = {
+    light = {
+        name = "Light",
+        hexCode = "#F4D5B8"
+    },
+    mediumLight = {
+        name = "Medium Light",
+        hexCode = "#EABFA5"
+    },
+    medium = {
+        name = "Medium",
+        hexCode = "#D1B89A"
+    },
+    mediumDark = {
+        name = "Medium Dark",
+        hexCode = "#BFA08E"
+    },
+    dark = {
+        name = "Dark",
+        hexCode = "#8D5B45"
+    }
+}
+
+return skinToneSettings
