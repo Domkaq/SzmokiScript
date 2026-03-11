faf74afe6c5b446247adfdb26a2dff81b045ed95
Domkaq <164402886+Domkaq@users.noreply.github.com>
2026-03-12 00:38:21 +0100

    Create core.lua for base functions and sensing

---
+++ modules/core.lua
@@ -0,0 +1,15 @@
+-- Base functions
+
+function getCurrentDateTime()
+    return os.date('%Y-%m-%d %H:%M:%S')
+end
+
+-- Sensing functions
+
+function senseEnvironment()
+    -- Code to sense the environment goes here.
+end
+
+function logSensingData(data)
+    -- Code to log sensing data goes here.
+end
