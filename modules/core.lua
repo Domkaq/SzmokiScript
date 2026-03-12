971b8c500c83469cd2b3a2c8e2e0aab857379268
Domkaq <164402886+Domkaq@users.noreply.github.com>
2026-03-12 01:12:01 +0100

    Update core.lua

--- modules/core.lua
+++ modules/core.lua
@@ -1,15 +1,13 @@
--- Base functions
+-- =====================================================
+-- Core modul - alap funkciók (jelenleg üres)
+-- =====================================================
 
-function getCurrentDateTime()
-    return os.date('%Y-%m-%d %H:%M:%S')
+local core = {}
+
+function core:init(config)
+    print("[CORE] Inicializálás...")
+    -- Ide jöhetnek később alap funkciók
+    return true
 end
 
--- Sensing functions
-
-function senseEnvironment()
-    -- Code to sense the environment goes here.
-end
-
-function logSensingData(data)
-    -- Code to log sensing data goes here.
-end
+return core
